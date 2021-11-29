c97734c58e95e7aa776530a3436d4a512a5aaf1f
nicolasmoises <86451624+nicolasmoises@users.noreply.github.com>
2021-11-29 19:46:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
 import data, { Cairns } from './data.js';
-
 function App() {
   return (
     <div className="App">
